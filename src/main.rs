c3198c3426b1bdc9efb1c4cdbf2da98ce1cbe93d
as-potehin <piroteh2006@yandex.ru>
2019-03-05 16:05:41 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+extern crate library;
+
+use library::mymod::{*};
+
+fn main() {
+    cortaje::main();
+    format::main();
+    arrays::main();
+    structure::main();;
+    list::main();
+    aliases::main();
+    enums::main();
+    destructure::cortaje::main();
+}
