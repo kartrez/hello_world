10675a032eae290534f56ba4b95f2df50ab4a80b
as-potehin <piroteh2006@yandex.ru>
2019-03-11 19:03:13 +0300

    + sep

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-
 extern crate library;
 
 use library::mymod::{*};
 
 fn main() {
+    println!("----------------START------------------\n");
     cortaje::main();
+    println!("---------------------------------------\n");
     format::main();
+    println!("---------------------------------------\n");
     arrays::main();
-    structure::main();;
+    println!("---------------------------------------\n");
+    structure::main();
+    println!("---------------------------------------\n");
     list::main();
+    println!("---------------------------------------\n");
     aliases::main();
+    println!("---------------------------------------\n");
     enums::main();
+    println!("---------------------------------------\n");
     destructure::cortaje::main();
-    println!("Hello World");
+    println!("---------------END---------------------\n");
 }
